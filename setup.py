0de1649f0942207ddd817d7850e1314c0f7ec826
0gl04q <115027096+0gl04q@users.noreply.github.com>
2025-02-02 16:27:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='db_session_manager',
     version='0.1',
-    packages=['session_maker'],
+    packages=find_packages(),
 )
